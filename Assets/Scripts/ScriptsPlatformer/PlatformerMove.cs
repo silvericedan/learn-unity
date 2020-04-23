cb4444b81d0cd4570ec276ef3d216a6a2e17a683
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-04-23 20:35:30 -0300

    Agregado Script PlatformerMove

---
+++ Assets/Scripts/ScriptsPlatformer/PlatformerMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMove : AButtonPressed
+{
+    Rigidbody2D rb;
+
+    public override void DownPressed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LeftPressed()
+    {
+        rb.AddForce(transform.right * -1);
+    }
+
+    public override void RightPressed()
+    {
+        rb.AddForce(transform.right * 1);
+    }
+
+    public override void UpPressed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PressRight();
+        PressLeft();
+    }
+
+}
