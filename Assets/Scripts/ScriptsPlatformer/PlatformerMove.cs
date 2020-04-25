865f0c81d7c401caf59352f4f7008890cf7f7040
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-04-24 21:52:32 -0300

    Agregado OnCollisionEnter en el control del Platformer

--- Assets/Scripts/ScriptsPlatformer/PlatformerMove.cs
+++ Assets/Scripts/ScriptsPlatformer/PlatformerMove.cs
@@ -6,6 +6,10 @@ public class PlatformerMove : AButtonPressed
 {
     Rigidbody2D rb;
 
+    private float impulse=0.3f;
+    private float lateral=1.0f;
+    public bool grounded;
+
     public override void DownPressed()
     {
         throw new System.NotImplementedException();
@@ -13,17 +17,37 @@ public class PlatformerMove : AButtonPressed
 
     public override void LeftPressed()
     {
-        rb.AddForce(transform.right * -1);
+        rb.AddForce(transform.right * -lateral);
     }
 
     public override void RightPressed()
     {
-        rb.AddForce(transform.right * 1);
+        rb.AddForce(transform.right * lateral);
     }
 
     public override void UpPressed()
     {
         throw new System.NotImplementedException();
+    }
+
+    public override void SpacePressed()
+    {
+        if (grounded)
+        {
+            rb.AddForce(transform.up * impulse, ForceMode2D.Impulse);
+            grounded = false;
+        }     
+    }
+
+    // esta funcion la llama Unity directamente cuando hay una colision con el objeto con la "tag" asignada en el inspector
+    // no se llama en start ni en update, solo se declara dentro del script y listo.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.gameObject.tag == "Terreno")
+        {
+            grounded = true;
+            Debug.Log("grounded");
+        }
     }
 
     // Start is called before the first frame update
@@ -42,6 +66,7 @@ public class PlatformerMove : AButtonPressed
     {
         PressRight();
         PressLeft();
+        PressSpace();
     }
 
 }
