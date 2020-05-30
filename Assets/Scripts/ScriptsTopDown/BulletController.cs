29a0b4ead915f1791b6d6f00cea87fedad0678d9
Eliseo Do Brito <eliseodb@gmail.com>
2020-05-30 13:15:00 -0300

    Las balas se destruyen si salen fuera de la cámara

---
+++ Assets/Scripts/ScriptsTopDown/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+	// Se ejecuta cuando el objeto queda fuera de la vista de cualquier cámara
+    void OnBecameInvisible()
+    {
+    	// Destruir el game object si el objeto queda fuera de la cámara
+    	Debug.Log("bala fuera de la pantalla, se eliminará");
+    	Destroy(gameObject);
+    }
+}
